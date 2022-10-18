d417a308e6707096cfc90505cff95841ba5eae25
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-10-18 20:11:21 +0300

    odified src v 1.0.1

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -1,9 +1,11 @@
 package page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -11,10 +13,15 @@ public class DashboardPage {
 
     private SelenideElement heading = $("[data-test-id=\"dashboard\"]");
     private ElementsCollection cards = $$(".list_item");
+    private ElementsCollection button = $$("[data-test-id=\"action-deposit\"]");
+
+    private ElementsCollection card1 = $$("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
+    private ElementsCollection card2 = $$("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = "р.";
 
     public DashboardPage() {
+        heading.shouldBe(visible);
     }
 
     public int getCardBalance() {
--- src/test/java/test/PageObjectsTest.java
+++ src/test/java/test/PageObjectsTest.java
@@ -1,15 +1,25 @@
 package test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import page.LoginPageV1;
 
+import javax.swing.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PageObjectsTest {
-    @Test
-    void shouldTransferMoneyTest() {
+    int money = 1000;
+
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
         var authInfo = DataHelper.getAuthInfo();
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         new LoginPageV1()
@@ -17,4 +27,12 @@ public class PageObjectsTest {
                 .validVerify(verificationCode);
     }
 
+    @Test
+    void shouldTransferMoneySecondCard() {
+        $("[data-test-id=\"action-deposit\"]").click();
+        $("[data-test-id=\"amount\"] input").setValue(String.valueOf(money));
+    }
 }
+    //$("[data-test-id=\"login\"] input").setValue(info.getLogin());
+        //$("[data-test-id=\"password\"] input").setValue(info.getPassword());
+        //$("[data-test-id=\"action-login\"]").click();
